2dc0b2b7326c10e5395fa4b346017a6908a54d63
Adarsh Gowda <34980180+adarshgowdaa@users.noreply.github.com>
2021-08-21 18:01:38 +0530

    Update Program-8.c

--- Program-8.c
+++ Program-8.c
@@ -2,9 +2,8 @@
 
 //Input: 5 2 3 4 5 6 
 //Output:Sum=20
-
 #include <stdio.h>
-void main() 
+int main() 
 {
    int arr1[10];
    int i,n, sum = 0;
@@ -19,8 +18,9 @@ void main()
 	  } 	
   pt = arr1; // pt store the base address of array arr1
   for (i = 0; i < n; i++) {
-      // write the code to calculate sum
-     
+      sum=sum+*pt;
+      pt++;// write the code to calculate sum
    }
   printf("Sum=%d",sum);
+  return 0;
 }
